f783a181f96e0296e399c49c13a61fe286cf2778
NikhilRameshHegde <nikhil36908@gmail.com>
2025-02-28 17:00:57 +0530

    Updated the api.js again.

--- combined FE/src/services/api.js
+++ combined FE/src/services/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
-
+ 
 // Updated base URL to include the '/api' prefix
 const baseUrl = import.meta.env.VITE_BASE_URL;
-
+ 
 // Get list of motor IDs
 export const getMotorIds = async () => {
-  const response = await axios.get(`${baseUrl}/motor-ids`);
+  const response = await axios.get(`${baseUrl}/api/motor-ids`);
   return response.data;
 };
-
+ 
 // Get failure trends for a given motor and a number of months
 export const getFailureTrends = async (motor_id, months) => {
   const response = await axios.get(
@@ -16,7 +16,7 @@ export const getFailureTrends = async (motor_id, months) => {
   );
   return response.data;
 };
-
+ 
 // Get RPM vs Load analytics for a given motor and a number of months
 export const getRpmVsLoad = async (motor_id, months) => {
   const response = await axios.get(
@@ -24,7 +24,7 @@ export const getRpmVsLoad = async (motor_id, months) => {
   );
   return response.data;
 };
-
+ 
 // Get Temperature vs Vibration analytics for a given motor and a number of months
 export const getTempVsVibration = async (motor_id, months) => {
   const response = await axios.get(
